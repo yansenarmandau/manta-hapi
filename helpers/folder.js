8a664ba0f16834d9949f343e0eb22481098c8b83
Yansen Armandau <yansenarmandau.dev@gmail.com>
2018-11-07 19:17:03 +0700

    eslint fix

--- helpers/folder.js
+++ helpers/folder.js
@@ -1,23 +1,23 @@
-'use strict'
+'use strict';
 
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 module.exports.isEmpty = (folder) => {
   try {
-    return !fs.readdirSync(folder).length
+    return !fs.readdirSync(folder).length;
   } catch (e) {
-    return false
+    return false;
   }
-}
+};
 
 module.exports.checkEmptyFolder = (folderName) => {
   if (!this.isEmpty(folderName)) {
     console.log(chalk.red(`
       Your destination folder "${chalk.green(path.resolve(folderName))}" is not empty.
       aborting!!
-    `))
-    process.exit(1)
+    `));
+    process.exit(1);
   }
-}
+};
